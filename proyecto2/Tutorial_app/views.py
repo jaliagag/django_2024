630af3dec283aba1ff5be90ebc3dcb2cc7be3be3
Jose Aliaga <jmfaliaga@gmail.com>
2024-06-10 08:57:58 -0300

    update

--- proyecto2/Tutorial_app/views.py
+++ proyecto2/Tutorial_app/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+import json
 from django.http import HttpResponse
 from tabulate import tabulate
 
-
+# https://wpdatatables.com/table-with-collapsible-rows/
 def inicio(request):
     table = [['one','two','three'],['four','five','six'],['seven','eight','nine']]
 
-    print(tabulate(table, tablefmt='html'))
-    nombre = {"nombre": "jose"}
-    return render(request, "Tutorial_app/index.html", nombre)
+    # make = tabulate(table, tablefmt='html')
+    with open(file='Tutorial_app/response_ocp.json') as f:
+        myj = json.loads(f.read())
+        nombre = {"data": table, "json": myj}
+        return render(request, "Tutorial_app/index.html", nombre )
     #return HttpResponse('vista inicio')
 
 def dashboards(request):
